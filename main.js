647898e2dee53bb607939d2db5ec3ca02921c15f
genildocs <genildocs@gmail.com>
2024-07-07 07:53:55 -0300

    commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import "./style.css";
-import { form } from "./modules/form.js";
-import { inputFormat } from "./modules/inputFormat.js";
-import Joi, { func } from "joi";
+import './style.css';
+import { form } from './modules/form.js';
+import { inputFormat } from './modules/inputFormat.js';
+import Joi, { func } from 'joi';
 
-document.querySelector("#app").innerHTML = `
+document.querySelector('#app').innerHTML = `
   <div>
     ${form}
     <div class="px-4 mt-4 bg-slate-500">
@@ -24,30 +24,35 @@ document.querySelector("#app").innerHTML = `
 
 const forms = document.forms[0];
 const formsElements = forms.elements;
-const btnSubmit = document.getElementById("calculate-repayments");
+const { amount, term, rate } = formsElements;
+const repayment = formsElements.mortgage[0];
+const interest = formsElements.mortgage[1];
+
+//buttons
+const btnSubmit = document.getElementById('calculate-repayments');
 inputFormat(formsElements);
 // functions
 function errorDisplay(erroForm, valueValidate) {
-  const elementsForm = ["amount", "term", "rate"];
+  const elementsForm = ['amount', 'term', 'rate'];
 
   elementsForm.forEach((el) => {
     const erroInput = document.getElementById(`${el}-error`);
     if (erroForm && erroForm.details.find((e) => e.context.key === el)) {
       erroInput.textContent = erroForm.details[0].message;
     } else {
-      erroInput.textContent = "";
+      erroInput.textContent = '';
     }
   });
 
-  const erroRadioInput = document.getElementById("radio-error");
+  const erroRadioInput = document.getElementById('radio-error');
   if (!valueValidate.repayment && !valueValidate.interest) {
-    erroRadioInput.textContent = "This field is required";
+    erroRadioInput.textContent = 'This field is required';
   } else {
-    erroRadioInput.textContent = "";
+    erroRadioInput.textContent = '';
   }
 }
 
-function validateForm(amount, term, rate, repayment, interest) {
+function validateForm() {
   const schema = Joi.object({
     amount: Joi.number().min(1).required(),
     term: Joi.number().min(1).max(50).required(),
@@ -67,31 +72,15 @@ function validateForm(amount, term, rate, repayment, interest) {
   return { error, value };
 }
 
-function calculteRepayments(formElement) {
-  const { amount, term, rate } = formElement;
-  const repayment = formElement.mortgage[0];
-  const interest = formElement.mortgage[1];
-
-  const { error, value } = validateForm(
-    amount,
-    term,
-    rate,
-    repayment,
-    interest
-  );
+function calculteRepayments(event) {
+  event.preventDefault();
+  const { error, value } = validateForm();
 
   if (!error) {
-    console.log("vamos calcular");
+    console.log('vamos calcular');
   }
 
   errorDisplay(error, value);
 }
 
-function submit() {
-  btnSubmit.addEventListener("click", function (e) {
-    e.preventDefault();
-    calculteRepayments(formsElements);
-  });
-}
-
-submit();
+btnSubmit.addEventListener('click', calculteRepayments);
