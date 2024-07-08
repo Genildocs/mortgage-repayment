a591d2ccebda91f0e3f85a009e4f6e66683bbab6
genildocs <genildocs@gmail.com>
2024-07-08 20:15:46 -0300

    commite

--- modules/calculate.js
+++ modules/calculate.js
@@ -1,8 +1,5 @@
 function calculateRepayments(value) {
-  const displayValue = document.getElementById('monthValue');
-  const displayTotValue = document.getElementById('totValue');
-  const displayCalculate = document.getElementById('completed');
-  const empty = document.getElementById('empty');
+
   let monthlyRepayment;
   let totalRepayment;
   const years = value.term * 12;
@@ -13,20 +10,32 @@ function calculateRepayments(value) {
       (value.amount * (ratePerMonth * Math.pow(1 + ratePerMonth, years))) /
       (Math.pow(1 + ratePerMonth, years) - 1);
     totalRepayment = monthlyRepayment * years;
-    displayValue.textContent = `£${monthlyRepayment.toLocaleString('en-US')}`;
-    displayTotValue.textContent = `£${totalRepayment.toLocaleString('en-US')}`;
-    displayCalculate.style.display = 'block';
-    empty.style.display = 'none';
+
+    contentValue(monthlyRepayment, totalRepayment)
+    handleToogle()
   } else {
     monthlyRepayment = (value.amount * (ratePerMonth / 100)) / 12;
     totalRepayment = value.amount + monthlyRepayment * years;
-    displayValue.textContent = `£${monthlyRepayment.toLocaleString('en-US')}`;
-    displayTotValue.textContent = `£${totalRepayment.toLocaleString('en-US')}`;
-    displayCalculate.style.display = 'block';
-    empty.style.display = 'none';
+
+    contentValue(monthlyRepayment, totalRepayment)
+    handleToogle()
   }
 }
 
-function displayCalculate() {}
+
+function contentValue(monthlyValue, totValue){
+  const displayValue = document.getElementById('monthValue');
+  const displayTotValue = document.getElementById('totValue');
+
+  displayValue.textContent = `$${monthlyValue.toLocaleString('en-US')}`;
+  displayTotValue.textContent = `$${totValue.toLocaleString('en-US')}`;
+}
+function handleToogle() {
+  const displayCalculate = document.getElementById('completed');
+  const empty = document.getElementById('empty');
+
+  displayCalculate.style.display = 'block';
+  empty.style.display = 'none';
+}
 
 export { calculateRepayments };
